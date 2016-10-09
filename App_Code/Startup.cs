ab0e7f35492e718cd551c8786b9fb8bfd805fc80
Paul Tutty <admin@codethirteen.com>
2016-10-08 19:36:53 -0500

    Fixes previous build failure

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capstone_Project.Startup))]
+namespace Capstone_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
